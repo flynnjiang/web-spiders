f34de4354a73454b2967975f1f0466c546365578
flynnjiang <flynnjiang@163.com>
2019-09-17 16:07:40 +0800

    更新cpppc.org的数据接口并适配python3

--- web-spiders/org.cpppc.py
+++ web-spiders/org.cpppc.py
@@ -1,50 +1,53 @@
 ﻿#!/usr/bin/python
 #coding=utf-8
 
-import urllib
-import urllib2
+import importlib
+
+import urllib.parse
+import urllib.request
+
 import json
 import time
 import sys
 
 
 def query_page(page_num):
-	req_url = "http://www.cpppc.org:8082/efmisweb/ppp/projectLivrary/getPPPList.do?tokenid=null"
-	req_data = {
-		'queryPage' : page_num,
-		'distStr' : '',
-		'induStr' : '',
-		'investStr' : '',
-		'projName' : '',
-		'sortby' : '',
-		'orderby' : '',
-		'stageArr' : ''
-	}
-	req_data_en = urllib.urlencode(req_data)
-	req = urllib2.Request(url = req_url, data = req_data_en)
-	rsp = urllib2.urlopen(req)
-	return rsp.read()
+	req_url = "http://www.cpppc.org:8086/pppcentral/map/getPPPList.do"
+	req_data = bytes(urllib.parse.urlencode({
+            'queryPage' : page_num,
+            'distStr' : '',
+            'induStr' : '',
+            'investStr' : '',
+            'publTimeStr' : '',
+            'projName' : '',
+            'sortby' : '',
+            'orderby' : '',
+            'stageArr' : '',
+            'projStateType' : 1
+        }), encoding='utf-8')
+	rsp = urllib.request.urlopen(url = req_url, data = req_data)
+	return rsp.read().decode('utf-8')
 
 	
 def main():
-	reload(sys)
-	sys.setdefaultencoding('utf-8')
+	importlib.reload(sys)
+	#sys.setdefaultencoding('utf-8')
 
 	# get pages
 	data = json.loads(query_page(1))
 	total_page = data['totalPage']
-	print "total_page = %d" % total_page
+	print("total_page = %d" % total_page)
 	
 	fp = open('ppp_projects_' + time.strftime('%Y%m%d-%H%M%S') + '.csv', 'w', 1)
 	fp.write('项目编号,项目名称,类别1,类别2,阶段,区域1,区域2,偿付模式,金额（万）,创建时间,更新时间,项目引用编号\n')
 
 	for i in range(1, total_page):
-		print "getting page %d..." % i
+		print("getting page %d/%d..." % (i, total_page))
 
 		data = json.loads(query_page(i))
 		projects = data['list']
 		for j in range(len(projects)):
-			print "saving project: " + projects[j]['PROJ_NAME'] 
+			print("saving project: " + projects[j]['PROJ_NAME'])
 			
 			# these maybe null
 			if (projects[j]['IVALUE2'] == None):
@@ -74,7 +77,7 @@ def main():
 
 	fp.close()
 
-	print "All done!"
+	print("All done!")
 	
 if __name__ == '__main__':
 	main()
